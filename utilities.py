40cc3dbdb44419c2a72b9c963bb6745a7e4f86aa
albert <albertlabonteiii3@gmail.com>
2023-03-27 10:33:52 -0700

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,5 +1,6 @@
 
 import heapq
+import math
 
 
 def random_choice_index(chances):
@@ -32,7 +33,27 @@ class PriorityQueue:
 
 
 def a_star_search(graph, start, goal):
-    # Implement A* pathfinding algorithm here
-    pass
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = dict()
+    cost_so_far = dict()
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next_node in graph.get_neighbors(current):
+            new_cost = cost_so_far[current] + graph.get_cost(current, next_node)
+            if next_node not in cost_so_far or new_cost < cost_so_far[next_node]:
+                cost_so_far[next_node] = new_cost
+                priority = new_cost + graph.heuristic(goal, next_node)
+                frontier.put(next_node, priority)
+                came_from[next_node] = current
+
+    return came_from, cost_so_far
 
 # Add any other utility functions or classes needed for the project here
